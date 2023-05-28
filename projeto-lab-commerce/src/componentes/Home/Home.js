5e13b80d5e88425e38095f611f4e15615f02fc24
danilogmarques <danilogustavomarques@gmail.com>
2023-05-28 19:41:45 -0300

    inserindo as lógicas do carrinho

--- projeto-lab-commerce/src/componentes/Home/Home.js
+++ projeto-lab-commerce/src/componentes/Home/Home.js
@@ -2,6 +2,16 @@ import planetas from "../../planetas.json";
 import { Imagem, PlanetasContainer } from "./style"
 console.log(planetas);
 const Home = ({ carrinho, setCarrinho }) => {
+
+    const adicionarPlanetaAoCarrinho = (planeta) => {
+        const planetaExisteNoCarrinho = carrinho.find((item) => item.nome === planeta.nome);
+
+        console.log(planetaExisteNoCarrinho);
+
+        setCarrinho([...carrinho, {...planeta, quantidade: 1}])    
+    };
+    
+
     return(
         <PlanetasContainer>
            {planetas.map((planeta) => {
@@ -10,7 +20,7 @@ const Home = ({ carrinho, setCarrinho }) => {
                         <Imagem src={planeta.imagem} alt={planeta.nome}/>
                         <p>Nome: {planeta.nome}</p>
                         <p>Preço: {planeta.valor}</p>
-                        <button>Comprar</button>
+                        <button onClick={() => adicionarPlanetaAoCarrinho(planeta)}>Comprar</button>
                     </div>
                 )
            })}      
