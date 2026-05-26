3d488e08698d5a5b75212eeb3222ae792624c3be
Danilo Marques <danilogustavomarques@gmail.com>
2026-05-26 15:41:44 -0300

    feat: foi implementada a barra de navegacao e tambem react-router

--- projeto-lab-commerce/src/App.js
+++ projeto-lab-commerce/src/App.js
@@ -1,65 +1,76 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  NavBar from "./componentes/NavBar/NavBar"
 import Carrinho from "./componentes/Carrinho/Carrinho";
 import Home from "./componentes/Home/Home";
-import Filtro from "./componentes/Filtro/Filtro";
 import { AppContainer } from "./style";
 import { useState } from "react";
-import { Theme } from "@radix-ui/themes";
 
 
 export default function App() {
+
     const [carrinho, setCarrinho] = useState([]);
-    const [ valorMin, setValorMin ] = useState(0);
-    const [ valorMax, setValorMax ] = useState(Infinity);
-    const [ valorInicial, setValorInicial ] = useState(0);
+    const [valorMin, setValorMin] = useState(0);
+    const [valorMax, setValorMax] = useState(Infinity);
 
-    const handleValorMin = (event) => {
-        setValorMin(event.target.value);
-    }
+    const adicionarPlanetaAoCarrinho = (planeta) => {
 
-    const handleValorMax = (event) => {
-        setValorMax(event.target.value);
-    }
+        setCarrinho([
+            ...carrinho,
+            { ...planeta, quantidade: 1 }
+        ]);
+    };
 
-     const adicionarPlanetaAoCarrinho = (planeta) => {
+    const removerPlanetaDoCarrinho = (item) => {
 
-        carrinho.find((item) => item.nome === planeta.nome);
+        const remove = carrinho.filter(
+            carrinho => carrinho !== item
+        );
 
-        setCarrinho([...carrinho, {...planeta, quantidade: 1}]) 
-    }
-        const removerPlanetaDoCarrinho = (item) => { 
-            const remove = carrinho.filter(carrinho => carrinho !== item)
-            setCarrinho(remove);
-        }
+        setCarrinho(remove);
+    };
 
-        // console.log(carrinho[0].valor);
-        const resultSoma = ({carrinho}) => {
-            for (let i = 0; i < carrinho.length; i++)
-            setValorInicial(carrinho[i].valor);
-        };
-        console.log(valorInicial);
-           
     return (
-        // <Theme>
-        <AppContainer>
-            {/* <Filtro 
-                valorMin={valorMin}
-                valorMax={valorMax}
-                handleValorMin={handleValorMin}
-                handleValorMax={handleValorMax}
-            /> */}
-            <Home 
-                carrinho={carrinho}
-                setCarrinho={setCarrinho}
-                valorMin={valorMin}
-                valorMax={valorMax}
-                adicionarPlanetaAoCarrinho={adicionarPlanetaAoCarrinho}
-            />
-            {/* <Carrinho 
-                carrinho={carrinho}
-                removerPlanetaDoCarrinho={removerPlanetaDoCarrinho}
-            /> */}
 
-        </AppContainer>
-        // </Theme>
+        <BrowserRouter>
+
+
+            <NavBar />
+
+            <AppContainer>
+
+                <Routes>
+
+                    <Route
+                        path="/"
+                        element={
+                            <Home
+                                carrinho={carrinho}
+                                setCarrinho={setCarrinho}
+                                valorMin={valorMin}
+                                valorMax={valorMax}
+                                adicionarPlanetaAoCarrinho={
+                                    adicionarPlanetaAoCarrinho
+                                }
+                            />
+                        }
+                    />
+
+                    <Route
+                        path="/carrinho"
+                        element={
+                            <Carrinho
+                                carrinho={carrinho}
+                                removerPlanetaDoCarrinho={
+                                    removerPlanetaDoCarrinho
+                                }
+                            />
+                        }
+                    />
+
+                </Routes>
+
+            </AppContainer>
+
+        </BrowserRouter>
     );
 }
--- projeto-lab-commerce/src/componentes/Home/Home.js
+++ projeto-lab-commerce/src/componentes/Home/Home.js
@@ -2,12 +2,16 @@ import planetas from "../../planetas.json";
 import { Imagem, PlanetasContainer } from "./style"
 import Card from "../Card/card";
 import CardContainer from "../Card/card";
+// 
 
 const Home = ({ valorMin, valorMax, adicionarPlanetaAoCarrinho  }) => {
 
     return(
-        
+        <div>
+            {/* <NavBar /> */}
             <CardContainer />
+
+        </div>
     
     )
 };
